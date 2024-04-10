5fee39fe4dcdfd6a3ae1bd7bde66ad7651bd86de
MD-DILDAR-MANDAL <dildarmandal335@gmail.com>
2024-04-10 20:41:43 +0530

    feat: add decryption

--- main.py
+++ main.py
@@ -21,8 +21,33 @@ def encryption():
  fin.close()
  print("Encryption Done...")
 
+#decryption
+def decryption():
+ path = input(r"Enter path of Image : ")
+ key = int(input("Enter Key for decryption of Image : "))
+ 
+ print('The path of file : ', path)
+ print('Key for encryption : ', key)
+
+ fin = open(path, "rb")
+ image = fin.read()
+ fin.close()
+
+ image = bytearray(image)
+
+ for index,values in enumerate(image):
+		 image[index] = values ^ key
+ 
+ fin = open(path, "wb")
+ fin.write(image)
+ fin.close()
+ print("decryption Done...")
+
+
 def main():
- encryption()
+ #encryption()
+ decryption()
+
 
 if __name__=="__main__":
  main()
